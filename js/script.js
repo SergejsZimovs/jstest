248d0542f8d5e7d431274ad6574aec61e049e123
sergejs zimovs <sergejs.zimovs97@gmail.com>
2025-02-15 17:34:31 +0200

    Js test

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Вам есть 18", "18");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваше Фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof answers);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 * 4 === 8);
+
+const isChecket = true,
+    isClose = true;
